8ebef0692fe9994b7dd841cb3b71a56af0bc8f48
Guilherme <sirguilhermeoliveira@gmail.com>
2020-11-30 19:52:00 -0300

    Versão finao 1.0

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -26,10 +26,10 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-mail') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Usuário') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                <input id="email" type="usuario" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
